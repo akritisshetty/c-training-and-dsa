fad34eb5c4801830c3c24b479a67d8e892b8553a
Akriti S Shetty <akritishetty.in@gmail.com>
2025-11-29 11:50:21 +0530

    29-11-2025

---
+++ recursionPrintName.c
@@ -0,0 +1,22 @@
+// Print name 5 times recursively
+
+#include<stdio.h>
+
+int b = 1;
+
+int name(int n, char a[10]) {
+	if(b>n) return 0;
+	printf("%s\n", a);
+	b++;
+	name(n, a);
+}
+
+int main(void) {
+	int n;
+	char a[10];
+	printf("Enter your name: \n");
+	scanf("%s", a);
+	printf("How many times do you want to print my name?\n");
+	scanf("%d", &n);
+	name(n, a);
+}
